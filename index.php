96bc6e73eb796edbe0a12423f8f47f3d2132429d
Abdul Hannan Sumon <49492875+ahsumon7@users.noreply.github.com>
2020-02-02 00:03:28 +0600

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
-
+<?php 
+$result="";
+if(isset($_POST['submit'])){
+require 'phpmailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
+$mail->Host='smtp.gmail.com';
+$mail->Port=587;
+$mail->SMTPAuth=true;
+$mail->SMTPSecure='tls';
+$mail->Username='abdulhannansumon70@gmail.com';
+$mail->Password='*isam78805577';
+$mail->setFrom($_POST['email'],$_POST['name']);
+$mail->addAddress('abdulhannansumon70@gmail.com');
+$mail->addReplyTo($_POST['email'],$_POST['name']);
+$mail->isHTML(true);
+$mail->Subject='Form Submission:' .$_POST['subject'];
+$mail->Body='<h3>Name :'.$_POST['name'].'<br> Email: '.$_POST['email'].'<br>Message: '.$_POST['msg'].'</h3>';
+if(!$mail->send()){
+$result = "Something went worng. Please try again.";
+}
+else {
+    $result="Thanks" .$_POST['name']. " for contacting us. We'll get back to you soon!";
+  }
+}
+?>
 
 
 
